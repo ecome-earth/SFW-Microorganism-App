780aa5a72ca8c4533c3b605cb7e755522ecc34df
Mahmoud ElWatidy <mahmoud.watidy@gmail.com>
2021-08-23 16:47:17 +0200

    ui: forgot password and authentucation message

--- lib/screens/AuthScreen.dart
+++ lib/screens/AuthScreen.dart
@@ -42,128 +42,174 @@ class _AuthScreen extends State<AuthScreen> {
                 right: 16.0,
                 bottom: 20.0,
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Row(),
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 60.0,
-                          backgroundColor: Colors.transparent,
-                          backgroundImage: AssetImage('assets/images/logo.png'),
-                        ),
-                        SizedBox(height: 10),
-                        Text('SFW Microorganisms',
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(fontWeight: FontWeight.bold,  fontSize: 20),
-                        ),
-                        SizedBox(height: 20),
-                        Form(
-                          key: _formKey,
-                          child: Center(
-                            child: Container(
-                              width: 220,
-                              child: Column(
-                              children: <Widget>[
-                                  // Add TextFormFields and ElevatedButton here.
-                                  TextFormField(
-                                    controller: email,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Email',
-                                    ),
-                                    validator: (String? value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter some text';
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  TextFormField(
-                                    obscureText: true,
-                                    controller: password,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Password',
-                                    ),
-                                    validator: (String? value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter some text';
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                ],
-                              ),
-                            )
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              textStyle: const TextStyle(fontSize: 16),
-                              minimumSize: Size(220, 34),
-                          ),
-                          onPressed: () {
-
-                            // CHECK IF EMAIL IS REGISTERED THEN LOGIN
-                            //    check email and password match
-                            //      yes ? get user data and update in local DB
-                            //      no ?
-                            // ELSE REGISTER
-                            print(email.text);
-                            print(password.text);
-
-
-                          },
-                          child: const Text('Login / Register'),
-                        ),
-                        const Divider(
-                          height: 10,
-                          thickness: 1,
-                          indent: 40,
-                          endIndent: 40,
-                        ),
-                        SignInButton(
-                          Buttons.Google,
-                          text: "Continue with Google",
-                          onPressed: () {},
-                        ),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: const EdgeInsets.all(16.0),
-                            primary: Colors.grey[800],
-                            textStyle: const TextStyle(fontSize: 12),
-                          ),
-                          onPressed: () {},
-                          child: const Text('skip login',
-                              style: TextStyle(fontSize: 14),
-
+              child: Center(
+                child: Container(
+                  width: 230,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CircleAvatar(
+                              radius: 60.0,
+                              backgroundColor: Colors.transparent,
+                              backgroundImage: AssetImage('assets/images/logo.png'),
                             ),
-                          ),
-                      ],
-                    ),
+                            SizedBox(height: 10),
+                            Text('SFW Microorganisms',
+                              textAlign: TextAlign.left,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,  
+                                fontSize: 20,
+                                
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                              child: Text('Invalid Email Address',
+                                style: TextStyle(
+                                  color: Colors.red
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Form(
+                              key: _formKey,
+                              child: Center(
+                                child: Container(
+                                  width: 220,
+                                  child: Column(
+                                    children: <Widget>[
+                                      // Add TextFormFields and ElevatedButton here.
+                                      TextFormField(
+                                        controller: email,
+                                        decoration: const InputDecoration(
+                                          hintText: 'Email',
+                                        ),
+                                        validator: (String? value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                      TextFormField(
+                                        obscureText: true,
+                                        controller: password,
+                                        decoration: const InputDecoration(
+                                          hintText: 'Password',
+                                        ),
+                                        validator: (String? value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  textStyle: const TextStyle(fontSize: 16),
+                                  minimumSize: Size(220, 34),
+                              ),
+                              onPressed: () {
+                
+                                // CHECK IF EMAIL IS REGISTERED THEN LOGIN
+                                //    check email and password match
+                                //      yes ? get user data and update in local DB
+                                //      no ?
+                                // ELSE REGISTER
+                                print(email.text);
+                                print(password.text);
+                
+                
+                              },
+                              child: const Text('Login / Register'),
+                            ),
+                            const Divider(
+                              height: 10,
+                              thickness: 1,
+                              indent: 40,
+                              endIndent: 40,
+                            ),
+                            SignInButton(
+                              Buttons.Google,
+                              text: "Continue with Google",
+                              onPressed: () {},
+                            ),
+                            Row(
+                              children: [
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    primary: Colors.grey[800],
+                                    textStyle: const TextStyle(fontSize: 10),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text('forgot password?',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.grey[600],
+                                      ),
+                                    ),
+                                  ),
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+                                    primary: Colors.grey[600],
+                                    textStyle: const TextStyle(fontSize: 10),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Text('skip login',
+                                      style: TextStyle(
+                                        fontSize: 14
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            // TextButton(
+                            //   style: TextButton.styleFrom(
+                            //     padding: const EdgeInsets.all(16.0),
+                            //     primary: Colors.grey[800],
+                            //     textStyle: const TextStyle(fontSize: 12),
+                            //   ),
+                            //   onPressed: () {},
+                            //   child: const Text('skip login',
+                            //       style: TextStyle(fontSize: 14),
+                            //     ),
+                            //   ),
+                          ],
+                        ),
+                      ),
+                      // FutureBuilder(
+                      //   future:
+                      //   GAuthentication.initializeFirebase(context: context),
+                      //   builder: (context, snapshot) {
+                      //     if (snapshot.hasError) {
+                      //       return Text('Error initializing Firebase');
+                      //     } else if (snapshot.connectionState ==
+                      //         ConnectionState.done) {
+                      //       return GoogleSignInButton();
+                      //     }
+                      //     return CircularProgressIndicator(
+                      //       valueColor: AlwaysStoppedAnimation<Color>(
+                      //         Colors.orange[400],
+                      //       ),
+                      //     );
+                      //   },
+                      // ),
+                    ],
                   ),
-                  // FutureBuilder(
-                  //   future:
-                  //   GAuthentication.initializeFirebase(context: context),
-                  //   builder: (context, snapshot) {
-                  //     if (snapshot.hasError) {
-                  //       return Text('Error initializing Firebase');
-                  //     } else if (snapshot.connectionState ==
-                  //         ConnectionState.done) {
-                  //       return GoogleSignInButton();
-                  //     }
-                  //     return CircularProgressIndicator(
-                  //       valueColor: AlwaysStoppedAnimation<Color>(
-                  //         Colors.orange[400],
-                  //       ),
-                  //     );
-                  //   },
-                  // ),
-                ],
+                ),
               ),
             ),
             // CustomBackButton(),
