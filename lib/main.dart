d9fe2c499cb9d25049871c1d031c87fbbcd46d91
Glitched15 <neellodha42@gmail.com>
2021-09-15 18:00:44 +0530

    UI : Forgot Password #18  - 1h

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sfw_microorganisms/classes/Profile.dart';
 
 import 'screens/AuthScreen.dart';
 import 'screens/ProfileScreen.dart';
 
 void main() {
-
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -19,86 +18,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SFW Microorganisms',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-        // Initialize FlutterFire
-        future: Firebase.initializeApp(),
-        builder: (context, snapshot) {
-          // Check for errors
+        title: 'SFW Microorganisms',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          inputDecorationTheme: InputDecorationTheme(
+            filled: true,
+            fillColor: const Color(0xffEEEEEE),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            hintStyle: TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+          ),
+          fontFamily: GoogleFonts.roboto().fontFamily,
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+          // Initialize FlutterFire
+          future: Firebase.initializeApp(),
+          builder: (context, snapshot) {
+            // Check for errors
 
-          if (snapshot.hasError) {
-            print('problem occured with Firebase');
+            if (snapshot.hasError) {
+              print('problem occured with Firebase');
 
+              // TODO: RETURN ERROR SCREEN INSTEAD OFF AuthScreen
+              return Scaffold(
+                  backgroundColor: Colors.grey[100],
+                  body: SafeArea(
+                      child: Stack(children: [
+                    Padding(
+                        padding: const EdgeInsets.only(
+                          left: 16.0,
+                          right: 16.0,
+                          bottom: 20.0,
+                        ),
+                        child: Center(
+                          child: Text('Problem with firebase ...'),
+                        )),
+                  ])));
+            }
 
-            // TODO: RETURN ERROR SCREEN INSTEAD OFF AuthScreen
-            return  Scaffold(
+            // Once complete, show your application
+            if (snapshot.connectionState == ConnectionState.done) {
+              // ADD LISTENER TO KNOW IF USER IS NOT LOGGED IN TO SIGNIN
+              print('All good and connected to firebase');
+              FirebaseAuth.instance.authStateChanges().listen((User? user) {
+                if (user == null) {
+                  // redirect to Login page if not logged in
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AuthScreen()),
+                  );
+                }
+                // else {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(builder: (context) => ProfileScreen()),
+                //   );
+                // }
+              });
+
+              return ProfileScreen();
+            }
+
+            // Otherwise, show something LOADING waiting for initialization to complete
+            return Scaffold(
                 backgroundColor: Colors.grey[100],
-                body:
-                SafeArea(
-                    child: Stack(
-                        children: [
-                          Padding(
-                              padding: const EdgeInsets.only(
-                                left: 16.0,
-                                right: 16.0,
-                                bottom: 20.0,
-                              ),
-                              child: Center(
-                                child: Text('Problem with firebase ...'),
-                              )
-                          ),
-                        ]
-                    )
-                )
-            );
-          }
-
-          // Once complete, show your application
-          if (snapshot.connectionState == ConnectionState.done) {
-
-            // ADD LISTENER TO KNOW IF USER IS NOT LOGGED IN TO SIGNIN
-            print('All good and connected to firebase');
-            FirebaseAuth.instance
-                .authStateChanges()
-                .listen((User? user) {
-                  if (user == null) {
-                    // redirect to Login page if not logged in
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AuthScreen()),
-                    );
-                  }
-                  // else {
-                  //   Navigator.push(
-                  //       context,
-                  //       MaterialPageRoute(builder: (context) => ProfileScreen()),
-                  //   );
-                  // }
-            });
-
-            return ProfileScreen();
-
-          }
-
-          // Otherwise, show something LOADING waiting for initialization to complete
-          return Scaffold(
-              backgroundColor: Colors.grey[100],
-              body:
-              SafeArea(
-              child: Stack(
-                children: [
+                body: SafeArea(
+                    child: Stack(children: [
                   Padding(
                       padding: const EdgeInsets.only(
                         left: 16.0,
@@ -107,20 +106,14 @@ class MyApp extends StatelessWidget {
                       ),
                       child: Center(
                         child: Text('Loading ...'),
-                      )
-                  ),
-                ]
-              )
-          )
+                      )),
+                ])));
+
+            // childAuthScreen();
+          },
+        )
+
+        //MyHomePage(title: 'SFW Microorganisms'),
         );
-
-          // childAuthScreen();
-
-        },
-      )
-
-         //MyHomePage(title: 'SFW Microorganisms'),
-    );
   }
-
 }
--- lib/screens/AuthScreen.dart
+++ lib/screens/AuthScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,18 +11,17 @@ import 'package:sfw_microorganisms/screens/ProfileScreen.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
 
-
-
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreen createState() => _AuthScreen();
 }
 
 class _AuthScreen extends State<AuthScreen> {
-
   final _formKey = GlobalKey<FormState>();
   final email = TextEditingController();
   final password = TextEditingController();
+
+  bool showpassword = false;
 
   var auth;
   var userStore;
@@ -32,7 +33,6 @@ class _AuthScreen extends State<AuthScreen> {
     super.initState();
     auth = FirebaseAuth.instance;
     userStore = FirebaseFirestore.instance.collection('users');
-    ;
   }
 
   @override
@@ -43,9 +43,7 @@ class _AuthScreen extends State<AuthScreen> {
     super.dispose();
   }
 
-
-  void showMessage (String text) {
-
+  void showMessage(String text) {
     final snackBar = SnackBar(
       content: Text(text),
     );
@@ -53,26 +51,21 @@ class _AuthScreen extends State<AuthScreen> {
     // Find the ScaffoldMessenger in the widget tree
     // and use it to show a
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    
   }
 
-  void continueToProfileScreen () {
+  void continueToProfileScreen() {
     Route route = MaterialPageRoute(builder: (context) => ProfileScreen());
     Navigator.pushReplacement(context, route);
   }
 
-  Future<void> resetPassword () async {
+  Future<void> resetPassword() async {
     if (isValidEmail(email.text)) {
       try {
-
         await auth.sendPasswordResetEmail(email: email.text);
         showMessage('We send you reset email');
-
       } catch (e) {
-
         print(e);
         showMessage("Couldn't connect to server");
-
       }
     } else {
       showMessage('Enter a valid email first');
@@ -80,25 +73,18 @@ class _AuthScreen extends State<AuthScreen> {
   }
 
   Future<void> getProfileAndContinueToProfileScreen() async {
-
     Profile? userProfile = await Profile.getByEmail(email.text);
 
-
     if (userProfile is Profile) {
-
       print('page navigation complete');
       continueToProfileScreen();
-
     } else {
-
       showMessage('Error occured gathering your profile');
       print('No profile gathered');
-
     }
   }
 
-  Future<void> createUserProfile (String email) async {
-
+  Future<void> createUserProfile(String email) async {
     // CREATE USER PROFILE
     Profile profile = new Profile();
     profile.name = 'Anonymous';
@@ -107,16 +93,12 @@ class _AuthScreen extends State<AuthScreen> {
     await Profile.add(profile.toJson());
   }
 
-
-  Future<void> registerWithEmail () async {
+  Future<void> registerWithEmail() async {
     try {
-
       print('CALLED REGISTER FUNCTION');
 
       UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-          email: email.text,
-          password: password.text
-      );
+          email: email.text, password: password.text);
 
       print('USER CREATED');
 
@@ -128,13 +110,10 @@ class _AuthScreen extends State<AuthScreen> {
 
       // print('EMAIL VERIFICATION SENT');
 
-
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-
         print('The password provided is too weak.');
         showMessage('The password provided is too§ weak.');
-
       }
       // else if (e.code == 'email-already-in-use') {
       //
@@ -146,19 +125,18 @@ class _AuthScreen extends State<AuthScreen> {
       else {
         print('error occured $e');
       }
-
     } catch (e) {
-
       print(e);
     }
   }
 
   Future<void> continueWithGoogle() async {
     // Trigger the authentication flow
-    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -167,7 +145,8 @@ class _AuthScreen extends State<AuthScreen> {
     );
 
     // Once signed in, return the UserCredential
-    UserCredential credentials = await FirebaseAuth.instance.signInWithCredential(credential);
+    UserCredential credentials =
+        await FirebaseAuth.instance.signInWithCredential(credential);
     User? user = credentials.user;
 
     // Create new profile if user profile exist
@@ -178,226 +157,199 @@ class _AuthScreen extends State<AuthScreen> {
     }
 
     continueToProfileScreen();
-
   }
 
-  Future<void> continueAnonymous () async {
+  Future<void> continueAnonymous() async {
     await FirebaseAuth.instance.signInAnonymously();
     createUserProfile(auth.currentUser.uid);
     continueToProfileScreen();
   }
 
-  Future<void> loginOrRegister () async {
+  Future<void> loginOrRegister() async {
     try {
       UserCredential userCredential = await auth.signInWithEmailAndPassword(
-          email: email.text,
-          password: password.text
-      );
+          email: email.text, password: password.text);
 
       // GET PPROFILE AND REDIRECT TO PROFILE PAGE
       getProfileAndContinueToProfileScreen();
-
-
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
-
         print('No user found for that email.');
         registerWithEmail();
-
-
       } else if (e.code == 'wrong-password') {
-
         showMessage('Invalid password');
         print('Wrong password provided for that user.');
-
       }
     }
   }
 
-  Future sendVerificationEmail () async {
+  Future sendVerificationEmail() async {
     User? user = auth.currentUser;
 
-    if (user!= null && !user.emailVerified) {
+    if (user != null && !user.emailVerified) {
       await user.sendEmailVerification();
       showMessage('Verfifcation Email Sent');
     }
   }
 
-  bool isValidEmail (String email) {
-    return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+  bool isValidEmail(String email) {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
   }
 
-  void signout()  {
+  void signout() {
     auth.signOut();
   }
 
   Future<void> handleSubmit() async {
-
     // LOGIN
-      // IF USER DOESN'T EXIST THEN CREATE PROFILE AND REGISTER
-      // ELSE GET PROFILE AND SHOW PROFILE SCREEN
+    // IF USER DOESN'T EXIST THEN CREATE PROFILE AND REGISTER
+    // ELSE GET PROFILE AND SHOW PROFILE SCREEN
 
     final FormState form = _formKey.currentState as FormState;
     if (form.validate()) {
-
-        await loginOrRegister();
-
-      }
-
+      await loginOrRegister();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body:
-      SafeArea(
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 16.0,
-                right: 16.0,
-                bottom: 20.0,
-              ),
-              child: Center(
-                child: Container(
-                  width: 230,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Row(),
-                      Expanded(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            CircleAvatar(
-                              radius: 60.0,
-                              backgroundColor: Colors.transparent,
-                              backgroundImage: AssetImage('assets/images/logo.png'),
-                            ),
-                            SizedBox(height: 10),
-                            Text('SFW Microorganisms',
-                              textAlign: TextAlign.left,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,  
-                                fontSize: 20,
-                                
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            Form(
-                              key: _formKey,
-                              child: Center(
-                                child: Container(
-                                  width: 220,
-                                  child: Column(
-                                    children: <Widget>[
-                                      // Add TextFormFields and ElevatedButton here.
-                                      TextFormField(
-                                        controller: email,
-                                        decoration: const InputDecoration(
-                                          labelText: 'Email',
-                                          hintText: 'Enter your email',
-                                        ),
-                                        validator: (String? value) {
-                                          if(
-                                            value != null && isValidEmail(value)
-                                          ) {
-                                            return null;
-                                          } else {
-                                            return "Invalid Email";
-                                          }
-                                        },
-                                      ),
-                                      TextFormField(
-                                        obscureText: true,
-                                        controller: password,
-                                        decoration: const InputDecoration(
-                                          labelText: 'Password',
-                                          hintText: 'Enter your password',
-                                        ),
-                                        validator: (String? value) {
-
-                                          if (value == null) return "You must enter a password";
-
-                                          bool atLeastEightChars = RegExp(r'.{8,}').hasMatch(value);
-                                          if (!atLeastEightChars) return "Enter at least 8 characters";
-
-                                          return null;
-
-                                          // TO ADD STRONG PASSWORD USE THE FOLLOWING REGEXP
-                                          // RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$').hasMatch(value)
-
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  textStyle: const TextStyle(fontSize: 16),
-                                  minimumSize: Size(220, 34),
-                              ),
-                              onPressed: handleSubmit,
-                              child: const Text('Login / Register'),
-                            ),
-                            const Divider(
-                              height: 10,
-                              thickness: 1,
-                              indent: 40,
-                              endIndent: 40,
-                            ),
-                            SignInButton(
-                              Buttons.Google,
-                              text: "Continue with Google",
-                              onPressed: continueWithGoogle,
-                            ),
-                            Row(
-                              children: [
-                                TextButton(
-                                  style: TextButton.styleFrom(
-                                    primary: Colors.grey[800],
-                                    textStyle: const TextStyle(fontSize: 10),
-                                  ),
-                                  onPressed: resetPassword,
-                                  child: Text('forgot password?',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.grey[600],
-                                      ),
-                                    ),
-                                  ),
-                                TextButton(
-                                  style: TextButton.styleFrom(
-                                    padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
-                                    primary: Colors.grey[600],
-                                    textStyle: const TextStyle(fontSize: 10),
-                                  ),
-                                  onPressed: continueAnonymous,
-                                  child: const Text('skip login',
-                                      style: TextStyle(
-                                        fontSize: 14
-                                      ),
-                                    ),
-                                  ),
-                              ],
-                            ),
-                          ],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height - 100,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    const Spacer(),
+                    Image.asset('assets/images/logo.png', height: 200),
+                    const Spacer(),
+                    TextFormField(
+                      controller: email,
+                      decoration: const InputDecoration(
+                        hintText: 'Username / Email',
+                      ),
+                      validator: (String? value) {
+                        if (value != null && isValidEmail(value)) {
+                          return null;
+                        } else {
+                          return "Invalid Email";
+                        }
+                      },
+                      cursorColor: Colors.black,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const Spacer(),
+                    TextFormField(
+                      obscureText: !showpassword,
+                      controller: password,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        suffixIcon: IconButton(
+                          onPressed: () => setState(() {
+                            showpassword = !showpassword;
+                          }),
+                          icon: showpassword
+                              ? Icon(Icons.visibility)
+                              : Icon(Icons.visibility_off),
                         ),
                       ),
-                      // FutureBuilder(
-                    ],
-                  ),
+                      validator: (String? value) {
+                        if (value == null) return "You must enter a password";
+
+                        bool atLeastEightChars =
+                            RegExp(r'.{8,}').hasMatch(value);
+                        if (!atLeastEightChars)
+                          return "Enter at least 8 characters";
+
+                        return null;
+                        // TO ADD STRONG PASSWORD USE THE FOLLOWING REGEXP
+                        // RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$').hasMatch(value)
+                      },
+                      cursorColor: Colors.black,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            primary: Colors.grey[800],
+                            textStyle: const TextStyle(fontSize: 10),
+                          ),
+                          onPressed: resetPassword,
+                          child: Text(
+                            'forgot password?',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Container(
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff03DAC5),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextButton(
+                        onPressed: handleSubmit,
+                        child: const Text(
+                          'Login / Register',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      width: 200,
+                      child: SignInButton(
+                        Buttons.Google,
+                        text: "Continue with Google",
+                        onPressed: continueWithGoogle,
+                      ),
+                    ),
+                    const Spacer(),
+                    // Row(
+                    //   children: [
+                    //     TextButton(
+                    //       style: TextButton.styleFrom(
+                    //         padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+                    //         primary: Colors.grey[600],
+                    //         textStyle: const TextStyle(fontSize: 10),
+                    //       ),
+                    //       onPressed: continueAnonymous,
+                    //       child: const Text(
+                    //         'skip login',
+                    //         style: TextStyle(fontSize: 14),
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
+                  ],
                 ),
               ),
             ),
-            // CustomBackButton(),
-          ],
+          ),
         ),
       ),
     );
