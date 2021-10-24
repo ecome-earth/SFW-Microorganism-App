9b707905965278fd46f23fe6ec6e63bc90447725
Dhruvil <pdhruvil1908@gmail.com>
2021-10-24 19:38:29 +0530

    Minor changes to bottom navbar

--- lib/components/bottom_navbar.dart
+++ lib/components/bottom_navbar.dart
@@ -1,6 +1,11 @@
-import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:sfw_microorganisms/models/quiz.dart';
 import 'package:sfw_microorganisms/providers/profile_provider.dart';
+import 'package:sfw_microorganisms/screens/gallery.dart';
+import 'package:sfw_microorganisms/screens/quiz/answer_selected_screen.dart';
+import 'package:sfw_microorganisms/screens/root_screen.dart';
 
 class CustomNavBar extends StatefulWidget {
   final ProfileProvider provider;
@@ -15,107 +20,74 @@ class CustomNavBar extends StatefulWidget {
 class _CustomNavBarState extends State<CustomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return FloatingNavbar(
-      backgroundColor: Color(0xFF5C3D2E),
-      selectedBackgroundColor: Color(0xFFFAEBE0),
-      selectedItemColor: Color(0xFF5C3D2E),
-      unselectedItemColor: Color(0xFFFAEBE0),
-      currentIndex: widget.index,
-      onTap: (index) {
+    return BottomNavyBar(
+      selectedIndex: widget.index,
+      itemCornerRadius: 30,
+      curve: Curves.bounceInOut,
+      onItemSelected: (index) {
         setState(() => widget.index = index);
         debugPrint('the nav items: ${widget.provider.bottomNavItems}');
-        widget.provider.selectItem(index: index);
-        widget.provider.setPreviousIndex(index: index);
+
+        switch (index) {
+          case 0:
+            print('Navigating to profile info screen');
+            // Navigator.of(context).pushNamed('root');
+            Navigator.push(
+              context,
+              PageTransition(
+                child: ProfileUploads(),
+                type: PageTransitionType.rightToLeftWithFade,
+                duration: Duration(milliseconds: 300),
+              ),
+            );
+            break;
+          case 1:
+            print('Navigating to quiz screen');
+            // Navigator.of(context).pushNamed('quiz');
+            Navigator.push(
+              context,
+              PageTransition(
+                child: AnswerSelectedScreen(),
+                type: PageTransitionType.rightToLeftWithFade,
+                duration: Duration(milliseconds: 300),
+              ),
+            );
+            break;
+          case 2:
+            print('Navigating to gallery');
+            // Navigator.pushNamed(context, 'gallery');
+            Navigator.push(
+              context,
+              PageTransition(
+                child: Gallery(),
+                type: PageTransitionType.rightToLeftWithFade,
+                duration: Duration(milliseconds: 300),
+              ),
+            );
+            break;
+          default:
+        }
       },
-      items: [
-        FloatingNavbarItem(
-          customWidget: InkWell(
-            onTap: () {
-              print('Navigating to profile info screen');
-              Navigator.of(context).pushNamed('root');
-              setState(() {
-                widget.index = 0;
-                widget.provider.bottomNavItems![0] = true;
-                widget.provider.bottomNavItems![1] = false;
-                widget.provider.bottomNavItems![2] = false;
-              });
-            },
-            child: Icon(
-              Icons.person,
-              size: 40,
-              color: widget.provider.bottomNavItems![0]!
-                  ? Color(0xFF5C3D2E)
-                  : Color(0xFFFAEBE0),
-            ),
-          ),
-          title: 'Profile',
+      items: <BottomNavyBarItem>[
+        BottomNavyBarItem(
+          icon: Icon(Icons.person),
+          title: Text('Profile'),
+          textAlign: TextAlign.center,
+          activeColor: Colors.blue,
         ),
-        FloatingNavbarItem(
-          customWidget: InkWell(
-            onTap: () {
-              print('Navigating to quiz screen');
-              Navigator.of(context).pushNamed('quiz');
-              setState(() {
-                widget.index = 1;
-                widget.provider.bottomNavItems![0] = false;
-                widget.provider.bottomNavItems![1] = true;
-                widget.provider.bottomNavItems![2] = false;
-              });
-              // provider.bottomNavItems![0]!;
-            },
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.quiz,
-                  size: 35,
-                  color: widget.provider.bottomNavItems![1]!
-                      ? Color(0xFF5C3D2E)
-                      : Color(0xFFFAEBE0),
-                ),
-                Text(
-                  'Quiz',
-                  style: TextStyle(
-                    color: widget.provider.bottomNavItems![1]!
-                        ? Color(0xFF5C3D2E)
-                        : Color(0xFFFAEBE0),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        BottomNavyBarItem(
+          icon: Icon(Icons.quiz_rounded),
+          title: Text('Quiz'),
+          textAlign: TextAlign.center,
+          activeColor: Colors.purple,
         ),
-        FloatingNavbarItem(
-          customWidget: InkWell(
-            onTap: () {
-              print('Navigating to gallery');
-              Navigator.pushNamed(context, 'gallery');
-              setState(() {
-                widget.index = 2;
-                widget.provider.bottomNavItems![0] = false;
-                widget.provider.bottomNavItems![1] = false;
-                widget.provider.bottomNavItems![2] = true;
-              });
-            },
-            child: Column(
-              children: [
-                Icon(
-                  Icons.style,
-                  size: 35,
-                  color: widget.provider.bottomNavItems![2]!
-                      ? Color(0xFF5C3D2E)
-                      : Color(0xFFFAEBE0),
-                ),
-                Text(
-                  'Gallery',
-                  style: TextStyle(
-                    color: widget.provider.bottomNavItems![2]!
-                        ? Color(0xFF5C3D2E)
-                        : Color(0xFFFAEBE0),
-                  ),
-                ),
-              ],
-            ),
+        BottomNavyBarItem(
+          icon: Icon(
+            Icons.photo_size_select_actual_outlined,
           ),
+          title: Text('Microdex'),
+          textAlign: TextAlign.center,
+          activeColor: Colors.amber,
         ),
       ],
     );
